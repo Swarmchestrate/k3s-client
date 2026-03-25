a6638bf744d26aa8e628fccdccd6f066c94e2653
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2026-03-25 12:36:16 +0000

    feat: update __all__ exports to include get_kubernetes_manifest in relevant modules

--- k3s_client/__init__.py
+++ k3s_client/__init__.py
@@ -1,5 +1,13 @@
-from .api.pods import PodManager
 from .api.applications import ApplicationManager
 from .cli.kubectl import Kubectl
+from .utils.manifest import get_kubernetes_manifest
 
-__all__ = ["PodManager", "ApplicationManager", "Kubectl"]
+
+__all__ = [
+    "PodManager",
+    "ApplicationManager",
+    "Kubectl",
+    "get_kubernetes_manifest",
+    
+    
+]
