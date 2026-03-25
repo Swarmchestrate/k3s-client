dde52345afc718bc9e4348405f73fcfaac924479
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2026-03-25 12:29:54 +0000

    feat: add detailed exception classes

--- k3s_client/exceptions.py
+++ k3s_client/exceptions.py
@@ -1,4 +1,21 @@
+# k3s_client/exceptions.py
+
 class K3sClientError(Exception):
     """Base exception for all k3s-client library errors."""
+    pass
 
+class DeploymentError(K3sClientError):
+    """Raised for deployment-specific errors."""
+    pass
+
+class ServiceError(K3sClientError):
+    """Raised for service-specific errors."""
+    pass
+
+class RegistrySecretError(K3sClientError):
+    """Raised for Docker registry secret issues."""
+    pass
+
+class ManifestError(K3sClientError):
+    """Raised for manifest creation or apply errors."""
     pass
