4452dd492580ffbb773ae9be7a0d3ad45ee2fdbf
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2026-04-01 11:45:59 +0000

    fix: correct PodManager export in __init__.py

--- k3s_client/__init__.py
+++ k3s_client/__init__.py
@@ -1,13 +1,12 @@
 from .api.applications import ApplicationManager
+from .api.pods import PodManager
 from .cli.kubectl import Kubectl
 from .utils.manifest import get_kubernetes_manifest
 
 
 __all__ = [
-    "PodManager",
     "ApplicationManager",
+    "PodManager",
     "Kubectl",
     "get_kubernetes_manifest",
-    
-    
 ]
