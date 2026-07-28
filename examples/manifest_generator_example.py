64aa63bfbdb31449cad28486f097f6903fc310c8
Gunjan Kotak <57281121+kotak-Gunjan@users.noreply.github.com>
2026-07-28 13:29:42 +0000

    refactor: reorder import statements for better organization

--- examples/manifest_generator_example.py
+++ examples/manifest_generator_example.py
@@ -1,7 +1,9 @@
-from k3s_client.utils.manifest import get_kubernetes_manifest
-from ruamel.yaml import YAML
 from io import StringIO
 from pathlib import Path
+
+from ruamel.yaml import YAML
+
+from k3s_client.utils.manifest import get_kubernetes_manifest
 
 TOSCA_FILE = "examples/tosca-file.yaml"  # path relative to example folder
 OUTPUT_FILE = "generated-manifests.yaml"
